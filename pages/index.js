8293ac75674944e45d6a0844b87b570b0e145893
Filippo Fortino <filippofortino@gmail.com>
2021-01-04 16:41:22 +0100

    wip

--- pages/index.js
+++ pages/index.js
@@ -52,19 +52,21 @@ export default function Home() {
               </p>
               <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-full shadow">
-                  <Link href="/" passHref>
-                    <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-full text-white bg-light-blue-600 hover:bg-light-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500 md:py-4 md:text-lg md:px-10">
-                      Scopri di più
-                    </a>
-                  </Link>
+                  <a
+                    href="#su-di-noi"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-full text-white bg-light-blue-600 hover:bg-light-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500 md:py-4 md:text-lg md:px-10"
+                  >
+                    Scopri di più
+                  </a>
                 </div>
 
                 <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <Link href="/#dove-siamo" passHref>
-                    <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-full text-light-blue-700 bg-light-blue-100 hover:bg-light-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500 md:py-4 md:text-lg md:px-10">
-                      Dove siamo
-                    </a>
-                  </Link>
+                  <a
+                    href="#dove-siamo"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-full text-light-blue-700 bg-light-blue-100 hover:bg-light-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500 md:py-4 md:text-lg md:px-10"
+                  >
+                    Dove siamo
+                  </a>
                 </div>
               </div>
             </div>
@@ -81,7 +83,7 @@ export default function Home() {
       </div>
 
       {/* Feature Section */}
-      <div className="py-16 bg-gray-50 overflow-hidden lg:py-24">
+      <div id="su-di-noi" className="py-16 bg-gray-50 overflow-hidden lg:py-24">
         <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
           <svg
             className="hidden lg:block absolute left-full transform -translate-x-1/2 -translate-y-1/4"
@@ -126,7 +128,10 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+          <div
+            id="i-nostri-servizi"
+            className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center"
+          >
             <div className="relative">
               <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
                 I nostri principali servizi
@@ -168,7 +173,7 @@ export default function Home() {
                         viewBox="0 0 24 24"
                         stroke="currentColor"
                         aria-hidden="true"
-                        stroke-width="0.4"
+                        strokeWidth={0.4}
                       >
                         <path
                           d="M21.219 2H2.78A.781.781 0 002 2.781V21.22c0 .431.35.781.781.781H21.22c.431 0 .781-.35.781-.781V2.78A.781.781 0 0021.219 2zM3.562 6.688h7.62c-.379 3.949-3.594 7.1-7.62 7.464V6.688zm0 13.75V15.86c1.818.525 2.889 2.653 3.086 4.576H3.563zm12.221 0H8.217c-.169-1.979-1.003-3.915-2.46-5.13C8.59 14.443 10.875 12.36 12 9.733c1.124 2.623 3.407 4.71 6.242 5.575-1.46 1.218-2.29 3.156-2.459 5.13zm4.655 0h-3.086c.196-1.914 1.261-4.05 3.085-4.577v4.576zm0-6.286c-4.026-.364-7.241-3.515-7.62-7.464h7.62v7.464zm0-9.027H3.563V3.562h16.875v1.563z"
@@ -215,7 +220,7 @@ export default function Home() {
                         viewBox="0 0 24 24"
                         stroke="currentColor"
                         aria-hidden="true"
-                        stroke-width="0.4"
+                        strokeWidth={0.4}
                       >
                         <path
                           d="M22.811 1.189a.645.645 0 00-.832-.068L8.65 10.704c-.4.288-.78.61-1.128.959a3.387 3.387 0 00-.725 1.073H4.061A3.06 3.06 0 001 15.797v.02a3.045 3.045 0 003.041 3.04h.02c.786 0 1.426.64 1.426 1.427s-.64 1.427-1.426 1.427h-.02a.645.645 0 000 1.289h.02a2.719 2.719 0 002.715-2.716 2.719 2.719 0 00-2.715-2.715h-.02a1.754 1.754 0 01-1.752-1.753v-.019a1.77 1.77 0 011.772-1.772h2.465c-.012.888.319 1.779.995 2.455a3.41 3.41 0 004.816 0 9.16 9.16 0 00.959-1.129l.953-1.326h3.275A2.027 2.027 0 0019.548 12a.644.644 0 10-1.289 0c0 .406-.33.736-.735.736h-2.348l7.703-10.715a.645.645 0 00-.068-.832zM12.25 14.599a7.88 7.88 0 01-.823.969 2.116 2.116 0 01-3.612-1.543h1.473a.642.642 0 001.098.502l1.38-1.38a.645.645 0 00-.911-.912l-.502.501H8.286a2.14 2.14 0 01.146-.162c.3-.3.626-.576.97-.823l10.13-7.283-7.283 10.13z"
@@ -334,7 +339,10 @@ export default function Home() {
             />
           </svg>
 
-          <div className="relative mt-12 sm:mt-16 lg:mt-24">
+          <div
+            id="la-nostra-storia"
+            className="relative mt-12 sm:mt-16 lg:mt-24"
+          >
             <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
               <div className="lg:col-start-2">
                 <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
@@ -404,7 +412,7 @@ export default function Home() {
       </div>
 
       {/* Where */}
-      <div className="bg-light-blue-700">
+      <div id="dove-siamo" className="bg-light-blue-700">
         <div className="pt-12 sm:pt-16 lg:pt-24">
           <div className="max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8">
             <div className="max-w-3xl mx-auto space-y-2 lg:max-w-none">
@@ -436,7 +444,7 @@ export default function Home() {
                     <div className="absolute top-6 left-6">
                       <h3
                         className="inline-flex px-4 py-1 rounded-full text-base font-semibold tracking-wide uppercase bg-light-blue-100 text-light-blue-600"
-                        id="tier-standard"
+                        id="pisa"
                       >
                         Pisa
                       </h3>
@@ -576,7 +584,7 @@ export default function Home() {
                     <div className="absolute top-6 left-6">
                       <h3
                         className="inline-flex px-4 py-1 rounded-full text-base font-semibold tracking-wide uppercase bg-light-blue-100 text-light-blue-600"
-                        id="tier-standard"
+                        id="fornacette"
                       >
                         Fornacette
                       </h3>
@@ -705,7 +713,7 @@ export default function Home() {
       </div>
 
       {/* Contacts */}
-      <div className="bg-white">
+      <div id="contatti" className="bg-white">
         <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
           <div className="divide-y-2 divide-gray-200">
             <div className="lg:grid lg:grid-cols-3 lg:gap-8">
@@ -806,55 +814,37 @@ export default function Home() {
           >
             <div className="px-5 py-2">
               <a
-                href="/"
+                href="#i-nostri-servizi"
                 className="text-base text-gray-500 hover:text-gray-900"
               >
-                About
+                Servizi
               </a>
             </div>
 
             <div className="px-5 py-2">
               <a
-                href="/"
+                href="#la-nostra-storia"
                 className="text-base text-gray-500 hover:text-gray-900"
               >
-                Blog
+                Storia
               </a>
             </div>
 
             <div className="px-5 py-2">
               <a
-                href="/"
+                href="#dove-siamo"
                 className="text-base text-gray-500 hover:text-gray-900"
               >
-                Jobs
+                Dove siamo
               </a>
             </div>
 
             <div className="px-5 py-2">
               <a
-                href="/"
+                href="#contatti"
                 className="text-base text-gray-500 hover:text-gray-900"
               >
-                Press
-              </a>
-            </div>
-
-            <div className="px-5 py-2">
-              <a
-                href="/"
-                className="text-base text-gray-500 hover:text-gray-900"
-              >
-                Accessibility
-              </a>
-            </div>
-
-            <div className="px-5 py-2">
-              <a
-                href="/"
-                className="text-base text-gray-500 hover:text-gray-900"
-              >
-                Partners
+                Contatti
               </a>
             </div>
           </nav>
@@ -890,53 +880,9 @@ export default function Home() {
                 />
               </svg>
             </a>
-
-            <a href="/" className="text-gray-400 hover:text-gray-500">
-              <span className="sr-only">Twitter</span>
-              <svg
-                className="h-6 w-6"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-              </svg>
-            </a>
-
-            <a href="/" className="text-gray-400 hover:text-gray-500">
-              <span className="sr-only">GitHub</span>
-              <svg
-                className="h-6 w-6"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
-
-            <a href="/" className="text-gray-400 hover:text-gray-500">
-              <span className="sr-only">Dribbble</span>
-              <svg
-                className="h-6 w-6"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
           </div>
           <p className="mt-8 text-center text-base text-gray-400">
-            &copy; 2020 Workflow, Inc. All rights reserved.
+            &copy; 2021 Lava&Lava.
           </p>
         </div>
       </footer>
