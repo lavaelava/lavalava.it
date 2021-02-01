c8f49596f0354550100784a668f8213dbd2ce0f0
Filippo Fortino <filippofortino@gmail.com>
2021-02-01 18:46:58 +0100

    Switch to preact at build time

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom': 'preact/compat',
+      })
+    }
+
+    return config
+  },
+}
