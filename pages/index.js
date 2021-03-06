6943dbc5bb2b27c28bbaaeb1d5cd35499de85d8b
Filippo Fortino <filippofortino@gmail.com>
2021-03-06 11:29:06 +0100

    add CTA secion

--- pages/index.js
+++ pages/index.js
@@ -916,9 +916,9 @@ export default function Home() {
           <div className="bg-white">
             <div className="max-w-4xl mx-auto py-16 px-4 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 lg:flex lg:items-center lg:justify-between">
               <h2 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                <span className="block">Ready to get started?</span>
+                <span className="block">Hai dei capi da lavare?</span>
                 <span className="block bg-gradient-to-r from-teal-500 to-cyan-600 bg-clip-text text-transparent">
-                  Get in touch or create an account.
+                  Veniamo noi da te o puoi portarli in lavanderia.
                 </span>
               </h2>
               <div className="mt-6 space-y-4 sm:space-y-0 sm:flex sm:space-x-5">
@@ -926,13 +926,13 @@ export default function Home() {
                   href="#"
                   className="flex items-center justify-center px-4 py-3 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400"
                 >
-                  Learn more
+                  Ordina online
                 </a>
                 <a
-                  href="#"
+                  href="#dove-siamo"
                   className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-cyan-800 bg-cyan-100 hover:bg-cyan-200"
                 >
-                  Get started
+                  Vieni in negozio
                 </a>
               </div>
             </div>
