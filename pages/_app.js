3dada1ccc5507529d9dc2baf4d39b9891b4c6d12
Filippo Fortino <filippofortino@gmail.com>
2021-03-23 17:08:41 +0100

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,22 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      const handleRouteChange = (url) => {
+        gtag.pageview(url)
+      }
+      router.events.on('routeChangeComplete', handleRouteChange)
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -11,6 +12,25 @@ class MyDocument extends Document {
       <Html lang="it">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            defer
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          {process.env.NODE_ENV === 'production' && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+              }}
+            />
+          )}
         </Head>
         <body className="antialiased">
           <Main />
