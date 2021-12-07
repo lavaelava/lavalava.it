c17013a4a260c713b0fd743c664b5d670f92e0f8
Filippo Fortino <filippofortino@gmail.com>
2021-12-07 20:04:44 +0100

    Remove "Promo Tappeti" slider

--- components/HeroSlider.js
+++ components/HeroSlider.js
@@ -63,57 +63,12 @@ export default function HeroSlider() {
                 </div>
               </div>
             </div>
-
-            <div className="relative shadow-xl keen-slider__slide sm:rounded-2xl sm:overflow-hidden">
-              <div className="absolute inset-0">
-                <Image
-                  className="absolute inset-0 object-cover w-full h-full"
-                  src="/images/woman-on-carpet.jpg"
-                  layout="fill"
-                  alt="Pila di vestiti su un tavolo"
-                />
-                <div className="absolute inset-0 bg-cyan-200 mix-blend-multiply" />
-              </div>
-              <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
-                <h1 className="text-4xl font-extrabold tracking-tight text-center sm:text-5xl lg:text-6xl">
-                  <span className="block text-white">
-                    Fai tornare il tuo tappeto
-                  </span>
-                  <span className="block text-cyan-200">come nuovo</span>
-                </h1>
-                <p className="max-w-lg mx-auto mt-6 text-xl text-center text-cyan-100 sm:max-w-3xl">
-                  Trattiamo tutte le tipologie di tappeti: orientali, persiani,
-                  in lana, sintetici ed altre. Laviamo il tuo tappeto ad acqua
-                  eliminando batteri, acari ed altre impurità. Approfitta ora
-                  della promozione!
-                </p>
-                <div className="max-w-sm mx-auto mt-10 sm:max-w-none sm:flex sm:justify-center">
-                  <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
-                    <Link href="/promo/tappeti-2021">
-                      <a className="flex items-center justify-center px-4 py-3 text-base font-medium bg-white border border-transparent rounded-md shadow-sm text-cyan-700 hover:bg-cyan-50 focus:outline-none focus:ring focus:ring-cyan-300 sm:px-8">
-                        Scopri di più
-                      </a>
-                    </Link>
-                    <a
-                      href="https://ordini.lavalava.it"
-                      className="flex items-center justify-center px-4 py-3 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-cyan-500 bg-opacity-60 hover:bg-opacity-70 focus:outline-none focus:ring focus:ring-white sm:px-8"
-                    >
-                      Ordina online
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
           {slider && (
             <nav
               className="flex items-center justify-center mt-12"
               aria-label="Slides"
             >
-              {/* <p className="text-sm font-medium">
-              Step {steps.findIndex((step) => step.status === 'current') + 1} of{' '}
-              {steps.length}
-            </p> */}
               <ol className="z-10 flex items-center space-x-5">
                 {[...Array(slider.details().size).keys()].map((idx) => (
                   <li key={idx}>
