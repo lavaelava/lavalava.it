9845a37424fa53fcd7a83761cfc95ea97029b9e2
Filippo Fortino <filippofortino@gmail.com>
2022-04-18 11:58:40 +0200

    Upgrade to tailwind v3

--- pages/promo/la-camicia-conveniente.js
+++ pages/promo/la-camicia-conveniente.js
@@ -20,7 +20,7 @@ export default function LaCamiciaConveniente() {
         <div className="mx-auto max-w-7xl lg:h-screen">
           <div className="relative z-10 h-full pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
             <svg
-              className="absolute inset-y-0 right-0 hidden w-48 h-full text-white transform translate-x-1/2 lg:block"
+              className="absolute inset-y-0 right-0 hidden w-48 h-full text-white translate-x-1/2 lg:block"
               fill="currentColor"
               viewBox="0 0 100 100"
               preserveAspectRatio="none"
